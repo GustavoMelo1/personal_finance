5a2a85b65ce4c47ae24ac0a6e771e68ca6a5f1c1
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-03-25 02:46:19 -0300

    fix: fixing the formatting and indentation

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,12 +1,11 @@
 import sqlite3
-import os
+
 
 def create_connection():
-
-    con = sqlite3.connect('data/financas.db')
+    con = sqlite3.connect("data/financas.db")
     cur = con.cursor()
 
-    cur.execute('''
+    cur.execute("""
         CREATE TABLE IF NOT EXISTS transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
                 data TEXT,
@@ -15,21 +14,21 @@ def create_connection():
                 tipo TEXT,
                 categoria TEXT
                 )
-    ''')
+    """)
 
-    cur.execute('''
+    cur.execute("""
         CREATE TABLE IF NOT EXISTS wishes (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 ativo TEXT,
                 quantidade INTEGER,
-                preco_medio REAL,
+                preco_medio REAL
                 )
-    ''')
+    """)
 
     con.commit()
     con.close()
-
     print("Database and tables created successfully.")
 
-    if __name__ == "__main__":
-        create_connection()
+
+if __name__ == "__main__":
+    create_connection()
