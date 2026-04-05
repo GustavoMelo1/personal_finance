a9045f45b6d1cf5ebb7a8f4128359dd5af9d778f
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-04-04 21:58:33 -0300

    refector: refactoring and adding logger

--- src/core/searcher.py
+++ src/core/searcher.py
@@ -1,36 +1,47 @@
 import json
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 #function to display wishes
 def wishes():
     try:
         with open('data/wishes.json', 'r', encoding='utf-8') as arquivo:
             dados = json.load(arquivo)
 
-        print("Searching items...\n")
-
-        for item in dados['wishes']:
-            print(f"Target: {item['search']}")
-            print(f"Stores: {', '.join(item['store'])}")
-            print(f"Max Value: R$ {item['max_value']:.2f}")
-            print("-" * 30)
+        logging.info("Searching wishes items...")
+        
+        return dados['wishes']
 
     except FileNotFoundError:
-        print("Wishes don't exist yet. Please add some wishes first.")
+        logging.warning("Wishes don't exist yet. Please add some wishes first.")
+        return []
+    except Exception as e:
+        logging.error(f"error in wishes: {e}")
+        return []
 
 def news():
     try:
         with open('data/financialmarketnews.json', 'r', encoding='utf-8') as arquivo:
             dados = json.load(arquivo)
             
-        print("Searching news...\n")
-    
-        for item in dados['news']:
-            print(f"words_for_search: {', '.join(item['words_for_search'])}")
-            print(f"news_sites: {', '.join(item['news_sites'])}")
-            print("-" * 30)
+        logging.info("Searching financial news...")
+        
+        # Devolve a lista de notícias
+        return dados['news']
             
     except FileNotFoundError:
-        print("News dont exist yet. Please add some news first.")
+        logging.warning("News don't exist yet. Please add some news first.")
+        return []
+    except Exception as e:
+        logging.error(f"error in news: {e}")
+        return []
 
 #executes the functions when the script runs
-wishes()
-news()
+if __name__ == "__main__":
+    wishes()
+    news()
