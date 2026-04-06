3a2e9205364235e1ec66a1fce6c6bc8a2dbf847d
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-04-06 20:10:30 -0300

    refactor: connection.py now only handles db connection

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,34 +1,7 @@
 import sqlite3
 
+PATH_db = 'data/financas.db'
 
 def create_connection():
-    con = sqlite3.connect("data/financas.db")
-    cur = con.cursor()
-
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS transactions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                data TEXT,
-                descricao TEXT,
-                valor REAL,
-                tipo TEXT,
-                categoria TEXT
-                )
-    """)
-
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS wishes (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                ativo TEXT,
-                quantidade INTEGER,
-                preco_medio REAL
-                )
-    """)
-
-    con.commit()
-    con.close()
-    print("Database and tables created successfully.")
-
-
-if __name__ == "__main__":
-    create_connection()
+    conn = sqlite3.connect(PATH_db)
+    return conn
