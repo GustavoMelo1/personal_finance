0551caa1a65763f8cac88420d595f6bcf64e73f4
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-04-06 21:28:14 -0300

    fix: replace logging with logger

--- src/core/searcher.py
+++ src/core/searcher.py
@@ -1,6 +1,8 @@
 import json
 import logging
 
+logger = logging.getLogger(__name__)
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -13,15 +15,15 @@ def wishes():
         with open('data/wishes.json', 'r', encoding='utf-8') as arquivo:
             dados = json.load(arquivo)
 
-        logging.info("Searching wishes items...")
+        logger.info("Searching wishes items...")
         
         return dados['wishes']
 
     except FileNotFoundError:
-        logging.warning("Wishes don't exist yet. Please add some wishes first.")
+        logger.warning("Wishes don't exist yet. Please add some wishes first.")
         return []
     except Exception as e:
-        logging.error(f"error in wishes: {e}")
+        logger.error(f"error in wishes: {e}")
         return []
 
 def news():
@@ -29,16 +31,16 @@ def news():
         with open('data/financialmarketnews.json', 'r', encoding='utf-8') as arquivo:
             dados = json.load(arquivo)
             
-        logging.info("Searching financial news...")
+        logger.info("Searching financial news...")
         
         # Devolve a lista de notícias
         return dados['news']
             
     except FileNotFoundError:
-        logging.warning("News don't exist yet. Please add some news first.")
+        logger.warning("News don't exist yet. Please add some news first.")
         return []
     except Exception as e:
-        logging.error(f"error in news: {e}")
+        logger.error(f"error in news: {e}")
         return []
 
 #executes the functions when the script runs
