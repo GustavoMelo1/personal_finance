b14acb7ec89a4654fd90d6bc226802b1cb30af29
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-06-13 11:44:35 -0300

    "feat: add GET /gastos endpoint with select_flow"

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from src.database.crud import select_flow
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "API funcionando"}
+
+@app.get("/gastos")
+def flow():
+    return {"my_flow": select_flow()}
