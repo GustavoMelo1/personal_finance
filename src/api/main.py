d4d4608532faf55265e87bf87efcf99f17a52882
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-06-23 22:59:16 -0300

    feat: add Flow pydantic model and POST /expenses endpoint skeleton

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from src.database.crud import select_flow, select_investment, select_wishes
 
 app = FastAPI()
+
+class Flow(BaseModel):
+    date: str
+    description:str
+    category:str
+    type:str
+    bank:str
+    value:float
 
 @app.get("/")
 def root():
@@ -11,6 +20,10 @@ def root():
 def flow():
     return {"flow": select_flow()}
 
+@app.post("/expenses")
+def create_flow(expense: Flow):
+    pass
+
 @app.get("/investments")
 def investments():
     return {"investments": select_investment()}
@@ -18,3 +31,4 @@ def investments():
 @app.get("/wishes")
 def wishes():
     return {"wishes": select_wishes()}
+
